98e834662b7eb367281e2b27c383467a439937fb
Himanshu <55566659+himanshu909009@users.noreply.github.com>
2020-03-22 13:49:33 +0530

    delet record

---
+++ CSE-C/HoliAssignment/Problem1/delet1.php
@@ -0,0 +1,11 @@
+<?php
+ include 'config1.php';
+ $enroll = $_GET['enroll'];
+
+ $q = "DELETE FROM `info2` WHERE enroll = $enroll " ;
+
+ mysqli_query($conn , $q);
+ header('location:show.php');
+
+
+?>
