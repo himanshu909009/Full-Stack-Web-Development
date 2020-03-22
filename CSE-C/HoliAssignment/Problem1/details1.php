80a32271f675569b4dfde86479880ecabc1ac7f3
Himanshu <55566659+himanshu909009@users.noreply.github.com>
2020-03-22 13:51:01 +0530

    showing details

---
+++ CSE-C/HoliAssignment/Problem1/details1.php
@@ -0,0 +1,55 @@
+<?php include('config1.php'); ?>
+
+<html>
+<head>
+<title>Records</title>
+<script>
+    
+</script>
+</head>
+<body bgcolor="#58D68D">
+ 
+<?php
+$sql = "SELECT * FROM `info2`";
+$result = mysqli_query($conn, $sql);
+
+if($result->num_rows > 0){
+?>  
+
+<table border="1" align="center" >
+
+    <thead>
+        <tr>
+            <th>enrollment_No.</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Course</th>
+            <th>Branch</th>
+
+            <th>Contact</th>
+            <th>E-Mail</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php    
+    while($data = $result->fetch_assoc()){ ?>
+    <tr>
+        <td><?php echo $data['enroll'];?></td>
+        <td><?php echo $data['name']; ?></td>
+        <td><?php echo $data['age']; ?></td>
+        <td><?php echo $data['course']; ?></td>
+        <td><?php echo $data['branch']; ?></td>
+        <td><?php echo $data['contact']; ?></td>
+        <td><?php echo $data['email']; ?></td>
+        <td> <a href="edit.php?id=<?php echo $data['id'];?>">Edit</a> </td>
+        <td> <a href="delete.php?id=<?php echo $data['id'];?>">Delete</a></td>
+    </tr>
+    <?php } ?>
+    </tbody>
+    
+<?php } ?>
+</table>   
+</body>   
+</html>
